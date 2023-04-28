7a4b08e8c4649a9252048f55d013056ad31d548b
Talyta Augusto <talitaaugustoaraujo@gmail.com>
2023-04-28 10:33:27 -0300

    Repositorio

---
+++ cadastro.js
@@ -0,0 +1,33 @@
+//###########################
+//Código de validação de formulário de Cadastro
+
+function Verificar(){
+    let nome = document.getElementById('nome').value;
+    let cpf = document.getElementById('CPF').value;
+    let email = document.getElementById('email').value;
+    let rg = document.getElementById('rg').value;
+    let data = document.getElementById('data').value;
+    let estado = document.getElementById('estado').value;
+    let cidade = document.getElementById('cidade').value;
+    let cep = document.getElementById('cep').value;
+    let bairro = document.getElementById('bairro').value;
+    let rua = document.getElementById('rua').value;
+    let numero = document.getElementById('numero').value;
+    let complemento = document.getElementById('complemento').value;
+    let senha = document.getElementById('senha').value;
+    let senhax = document.getElementById('senhax').value;
+
+    if(!nome || !cpf || !email || !rg || !data || !estado || !cidade || !cep || !bairro || !rua || !numero ||!complemento || !senha || !senhax){
+        alert("Campos de preenchimento obrigatório. Favor preencher");
+    }else{
+        alert("Campos preenchidos com ");
+    }
+    if((senha2 !== '' && senha !== '') && senha2 == senha){
+    }else{
+        alert("Sua senha esta incorreta");
+    }
+    }
+
+
+
+//#############################
